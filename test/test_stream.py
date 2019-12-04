e623877920216be8b2c51cc446fba638c994ff58
sgaron-cse <sgaron.cse@gmail.com>
2019-12-04 11:18:56 -0500

    Fix stream search test by setting the order after the fact. The order is not what we are testing, we are testing the data.

--- test/test_stream.py
+++ test/test_stream.py
@@ -1,42 +1,42 @@
 
 
 def test_alert(datastore, client):
-    res = [x for x in client.search.stream.alert("id:*")]
+    res = sorted([x for x in client.search.stream.alert("id:*")], key=lambda k: k['id'])
     assert len(res) > 0
-    assert res == list(datastore.alert.stream_search("id:*", as_obj=False))
+    assert res == sorted(list(datastore.alert.stream_search("id:*", as_obj=False)), key=lambda k: k['id'])
 
 
 def test_file(datastore, client):
-    res = [x for x in client.search.stream.file("id:*")]
+    res = sorted([x for x in client.search.stream.file("id:*")], key=lambda k: k['id'])
     assert len(res) > 0
-    assert res == list(datastore.file.stream_search("id:*", as_obj=False))
+    assert res == sorted(list(datastore.file.stream_search("id:*", as_obj=False)), key=lambda k: k['id'])
 
 
 def test_heuristic(datastore, client):
-    res = [x for x in client.search.stream.heuristic("id:*")]
+    res = sorted([x for x in client.search.stream.heuristic("id:*")], key=lambda k: k['id'])
     assert len(res) > 0
-    assert res == list(datastore.heuristic.stream_search("id:*", as_obj=False))
+    assert res == sorted(list(datastore.heuristic.stream_search("id:*", as_obj=False)), key=lambda k: k['id'])
 
 
 def test_result(datastore, client):
-    res = [x for x in client.search.stream.result("id:*")]
+    res = sorted([x for x in client.search.stream.result("id:*")], key=lambda k: k['id'])
     assert len(res) > 0
-    assert res == list(datastore.result.stream_search("id:*", as_obj=False))
+    assert res == sorted(list(datastore.result.stream_search("id:*", as_obj=False)), key=lambda k: k['id'])
 
 
 def test_signature(datastore, client):
-    res = [x for x in client.search.stream.signature("id:*")]
+    res = sorted([x for x in client.search.stream.signature("id:*")], key=lambda k: k['id'])
     assert len(res) > 0
-    assert res == list(datastore.signature.stream_search("id:*", as_obj=False))
+    assert res == sorted(list(datastore.signature.stream_search("id:*", as_obj=False)), key=lambda k: k['id'])
 
 
 def test_submission(datastore, client):
-    res = [x for x in client.search.stream.submission("id:*")]
+    res = sorted([x for x in client.search.stream.submission("id:*")], key=lambda k: k['id'])
     assert len(res) > 0
-    assert res == list(datastore.submission.stream_search("id:*", as_obj=False))
+    assert res == sorted(list(datastore.submission.stream_search("id:*", as_obj=False)), key=lambda k: k['id'])
 
 
 def test_workflow(datastore, client):
-    res = [x for x in client.search.stream.workflow("id:*")]
+    res = sorted([x for x in client.search.stream.workflow("id:*")], key=lambda k: k['id'])
     assert len(res) > 0
-    assert res == list(datastore.workflow.stream_search("id:*", as_obj=False))
+    assert res == sorted(list(datastore.workflow.stream_search("id:*", as_obj=False)), key=lambda k: k['id'])
