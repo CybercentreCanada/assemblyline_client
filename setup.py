4718cfbf8bbfe0a27400289bcc951ddd8fe3149a
Steve Garon <steve.garon@cyber.gc.ca>
2020-09-28 09:34:02 -0400

    Change development status to stable

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries',
         'License :: OSI Approved :: MIT License',
