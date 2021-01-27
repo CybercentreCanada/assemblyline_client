6e4ee02af8e18b6d3137472838c6962cf5172a66
Steve Garon <steve.garon@cyber.gc.ca>
2021-01-27 11:04:19 -0500

    Make sure submit tests are consistent

--- test/test_submit.py
+++ test/test_submit.py
@@ -22,6 +22,7 @@ def test_submit_content(datastore, client):
     content = get_random_phrase(wmin=15, wmax=50).encode()
     fname = "{}.txt".format(get_random_id())
     res = client.submit(content=content, fname=fname)
+    assert res is not None
     assert res['sid'] is not None
     assert res['files'][0]['name'] == fname
     assert res == datastore.submission.get(res['sid'], as_obj=False)
@@ -35,6 +36,7 @@ def test_submit_path(datastore, client):
 
     params = {'service_spec': {"extract": {"password": "test"}}}
     res = client.submit(path=test_path, params=params)
+    assert res is not None
     assert res.get('sid', None) is not None
     assert res['files'][0]['name'] == os.path.basename(test_path)
     assert res['params']['service_spec'] == params['service_spec']
@@ -45,6 +47,7 @@ def test_submit_sha(datastore, client):
     file_id = random_id_from_collection(datastore, 'file')
     metadata = {"file_id": get_random_id(), "comment": "test"}
     res = client.submit(sha256=file_id, metadata=metadata)
+    assert res is not None
     assert res['sid'] is not None
     assert res['files'][0]['sha256'] == file_id
     assert res['metadata'] == metadata
@@ -52,9 +55,10 @@ def test_submit_sha(datastore, client):
 
 
 def test_submit_url(datastore, client):
-    url='https://www.cyber.gc.ca/en/theme-gcwu-fegc/assets/wmms.svg'
+    url = 'https://www.cyber.gc.ca/en/theme-gcwu-fegc/assets/wmms.svg'
     params = {"deep_scan": True, "ignore_cache": True, "priority": 100}
     res = client.submit(url=url, params=params)
+    assert res is not None
     assert res['sid'] is not None
     assert res['files'][0]['name'] == os.path.basename(url)
     for k in params:
@@ -68,11 +72,12 @@ def test_submit_dynamic(datastore, client):
 
     res = client.submit.dynamic(submission_data['files'][0]['sha256'], copy_sid=submission_id,
                                 name=submission_data['files'][0]['name'])
+    assert res is not None
     assert res['sid'] is not None
     assert res == datastore.submission.get(res['sid'], as_obj=False)
     assert 'Dynamic Analysis' in res['params']['services']['selected']
     for k in res['params']:
-        if k not in ['submitter', 'services', 'description']:
+        if k not in ['submitter', 'services', 'description', 'quota_item']:
             assert res['params'][k] == submission_data['params'][k]
 
 
@@ -82,8 +87,9 @@ def test_resubmit(datastore, client):
     submission_data['params']['services']['runtime_excluded'] = []
 
     res = client.submit.resubmit(submission_id)
+    assert res is not None
     assert res['sid'] is not None
     assert res == datastore.submission.get(res['sid'], as_obj=False)
     for k in res['params']:
-        if k not in ['submitter', 'description']:
+        if k not in ['submitter', 'description', 'quota_item']:
             assert res['params'][k] == submission_data['params'][k]
