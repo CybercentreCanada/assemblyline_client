2f5a2e7e5ea441152fe47576802aa09e97026a56
Steve Garon <steve.garon@cyber.gc.ca>
2021-03-31 20:10:06 +0000

    Added support for missing user APIs

--- assemblyline_client/v4_client/module/user.py
+++ assemblyline_client/v4_client/module/user.py
@@ -1,28 +1,197 @@
-from assemblyline_client.v4_client.common.utils import api_path, api_path_by_module
+from assemblyline_client.v4_client.common.utils import api_path, api_path_by_module, get_function_kwargs
 
 
-class User(object):
+class Avatar(object):
     def __init__(self, connection):
         self._connection = connection
 
     def __call__(self, username):
         """\
-Return the settings for the given username.
+Loads the user's avatar.
+
+Required:
+username    : User key (string)
+
+Throws a Client exception if the user does not exist.
+"""
+        return self._connection.get(api_path('user', 'avatar', username))
+
+    def update(self, username, avatar):
+        """\
+Update the user's avatar.
+
+Required:
+username    : User key (string)
+avatar      : New avatar for the user
+
+Throws a Client exception if the user does not exist.
+"""
+        return self._connection.post(api_path('user', 'avatar', username), data=avatar)
+
+
+class Favorites(object):
+    def __init__(self, connection):
+        self._connection = connection
+
+    def __call__(self, username):
+        """\
+Loads the user's favorites.
+
+Required:
+username    : User key (string)
+
+Throws a Client exception if the user does not exist.
+"""
+        return self._connection.get(api_path('user', 'favorites', username))
+
+    def add(self, username, fav_type, fav_data):
+        """\
+Add a favorite to the user's favorites.
+
+Required:
+username    : User key (string)
+fav_type    : Type of favorite to remove (string)
+fav_data    : Data block of the favority to add (dict)
+
+Throws a Client exception if the user does not exist.
+"""
+        return self._connection.put(api_path('user', 'favorites', username, fav_type), json=fav_data)
+
+    def delete(self, username, fav_type, name):
+        """\
+Remove a favorite from the user's favorites.
+
+Required:
+username    : User key (string)
+fav_type    : Type of favorite to remove (string)
+name        : Name of favorite to remove (string)
+
+Throws a Client exception if the user does not exist.
+"""
+        return self._connection.delete(api_path('user', 'favorites', username, fav_type), data=name)
+
+    def update(self, username, favorites):
+        """\
+Update the user's favorite queries.
+
+Required:
+username    : User key (string)
+favorites   : New favorites for the user
+
+Throws a Client exception if the user does not exist.
+"""
+        return self._connection.post(api_path('user', 'favorites', username), json=favorites)
+
+
+class Settings(object):
+    def __init__(self, connection):
+        self._connection = connection
+
+    def __call__(self, username):
+        """\
+Get the current user's settings.
+
+Required:
+username    : User key (string)
+"""
+        return self._connection.get(api_path('user', 'settings', username))
+
+    def update(self, username, settings):
+        """\
+Update the user's settings.
+
+Required:
+username    : User key (string)
+settings    : New settings for the user
+
+Throws a Client exception if the user does not exist.
+"""
+        return self._connection.post(api_path('user', 'settings', username), json=settings)
+
+
+class User(object):
+    def __init__(self, connection):
+        self._connection = connection
+        self.avatar = Avatar(connection)
+        self.favorites = Favorites(connection)
+        self.settings = Settings(connection)
+
+    def __call__(self, username):
+        """\
+Return the profile for the given username.
 
 Required:
 username: User key. (string).
 
-Throws a Client exception if the submission does not exist.
+Throws a Client exception if the user does not exist.
 """
         return self._connection.get(api_path('user', username))
+
+    def add(self, username, user_data):
+        """\
+Add a user to the system
+
+Required:
+username    : Name of the user to add to the system
+user_data   : Profile data of the user to add
+"""
+        return self._connection.put(api_path('user', username), json=user_data)
+
+    def delete(self, username):
+        """\
+Remove the account specified by the username.
+
+Required:
+username    : Name of the user to remove from the system
+"""
+        return self._connection.delete(api_path('user', username))
+
+    def list(self, query="*:*", rows=10, offset=0, sort="uname asc"):
+        """\
+List users of the system (per page)
+
+Required:
+offset     : Offset in the user bucket
+query      : Filter to apply to the user list
+rows       : Max number of user returned
+sort       : Sort order
+"""
+        return self._connection.get(api_path_by_module(self, **get_function_kwargs('self')))
 
     def submission_params(self, username):
         """\
 Return the submission parameters for the given username.
 
 Required:
-username: User key. (string).
+username    : User key (string)
 
-Throws a Client exception if the submission does not exist.
+Throws a Client exception if the user does not exist.
 """
         return self._connection.get(api_path_by_module(self, username))
+
+    def tos(self, username):
+        """\
+Specified user send agreement to Terms of Service
+
+Required:
+username    : Username of the user that agrees with tos (string)
+
+Throws a Client exception if the user does not exist.
+"""
+        return self._connection.get(api_path_by_module(self, username))
+
+    def update(self, username, user_data):
+        """\
+Update a user profile in the system.
+
+Required:
+username    : Name of the user to update in the system
+user_data   : Profile data of the user to update
+"""
+        return self._connection.post(api_path('user', username), json=user_data)
+
+    def whoami(self):
+        """\
+Return the currently logged in user as well as the system configuration.
+"""
+        return self._connection.get(api_path_by_module(self))
--- test/test_user.py
+++ test/test_user.py
@@ -1,8 +1,15 @@
+import pytest
+import random
+
+from assemblyline_client.v4_client.common.utils import ClientError
 
 try:
+    from assemblyline.odm.models.user_favorites import Favorite
+    from assemblyline.odm.models.user import User
+    from assemblyline.odm.random_data import random_model_obj
+
     from utils import random_id_from_collection
 except ImportError:
-    import pytest
     import sys
     if sys.version_info < (3, 0):
         pytestmark = pytest.mark.skip
@@ -10,7 +17,98 @@ except ImportError:
         raise
 
 
-def test_get_user(datastore, client):
+def test_add(datastore, client):
+    user_id = 'to_be_added'
+
+    user_data = random_model_obj(User, as_json=True)
+    user_data['uname'] = user_id
+
+    assert datastore.user.get(user_id, as_obj=False) is None
+
+    res = client.user.add(user_id, user_data)
+    assert res['success']
+
+    assert datastore.user.get(user_id, as_obj=False) == user_data
+
+
+def test_avatar(datastore, client):
+    # Test adding avatar (1x1 white gif)
+    new_avatar = "data:image/gif;base64,R0lGODdhAQABAIABAP///wAAACwAAAAAAQABAAACAkQBADs="
+    res = client.user.avatar.update('admin', new_avatar)
+    assert res['success']
+
+    # Test getting the user avatar
+    avatar = client.user.avatar('admin')
+    assert avatar == new_avatar
+
+    with pytest.raises(ClientError):
+        client.user.avatar.update('user', new_avatar)
+
+
+def test_favorites(datastore, client):
+    possible_fav_types = {'alert', 'error', 'search', 'signature', 'submission'}
+    user_id = random_id_from_collection(datastore, 'user')
+
+    # Test getting favorites
+    favorites = client.user.favorites(user_id)
+    assert possible_fav_types.issubset(set(favorites.keys()))
+
+    # Test adding favorites
+    fav_type = random.choice(list(possible_fav_types))
+    new_fav = random_model_obj(Favorite, as_json=True)
+    res = client.user.favorites.add(user_id, fav_type, new_fav)
+    assert res['success']
+
+    # Test if favorite was added
+    found = False
+    for fav in datastore.user_favorites.get(user_id, as_obj=False)[fav_type]:
+        if fav['name'] == new_fav['name'] and fav['query'] == new_fav['query']:
+            found = True
+            break
+    assert found
+
+    # Test delete favorite
+    client.user.favorites.delete(user_id, fav_type, new_fav['name'])
+    assert res['success']
+
+    # Test if favorite was deleted
+    found = False
+    for fav in datastore.user_favorites.get(user_id, as_obj=False)[fav_type]:
+        if fav['name'] == new_fav['name'] and fav['query'] == new_fav['query']:
+            found = True
+            break
+    assert not found
+
+    # Test update favorites
+    new_favs = {
+        'alert': [new_fav],
+        'error': [new_fav],
+        'search': [new_fav],
+        'signature': [new_fav],
+        'submission': [new_fav]
+    }
+    res = client.user.favorites.update(user_id, new_favs)
+    assert res['success']
+
+    # Test is favorites were really updated
+    for favs in datastore.user_favorites.get(user_id, as_obj=False).values():
+        assert favs[0]['name'] == new_fav['name']
+
+
+def test_delete(datastore, client):
+    user_id = 'to_be_deleted'
+    user_data = random_model_obj(User)
+    user_data['uname'] = user_id
+    datastore.user.save(user_id, user_data)
+    assert datastore.user.get(user_id, as_obj=False) is not None
+
+    res = client.user.delete(user_id)
+    assert res['success']
+
+    assert datastore.user.get(user_id, as_obj=False) is None
+
+
+def test_get(datastore, client):
     user_id = random_id_from_collection(datastore, 'user')
     user_data = datastore.user.get(user_id, as_obj=False)
 
@@ -24,7 +122,34 @@ def test_get_user(datastore, client):
     assert sorted(res['security_tokens']) == sorted(list(user_data['security_tokens'].keys()))
 
 
-def test_get_submission_params(datastore, client):
+def test_list(datastore, client):
+    res = client.user.list()
+    assert 'admin' in [x['uname'] for x in res['items']]
+
+    res = client.user.list(query="id:admin")
+    assert 'admin' in [x['uname'] for x in res['items']]
+    assert res['total'] == 1
+
+
+def test_settings(datastore, client):
+    user_id = random_id_from_collection(datastore, 'user')
+
+    # Test getting settings
+    settings = client.user.settings(user_id)
+    assert {'classification', 'download_encoding', 'priority', 'ttl'}.issubset(set(settings.keys()))
+
+    # Test updating settings
+    new_desc = 'NEW_DEFAULT_DESCRIPTION'
+    settings['description'] = new_desc
+
+    res = client.user.settings.update(user_id, settings)
+    assert res['success']
+
+    # Validate changes were applied
+    assert new_desc == datastore.user_settings.get(user_id, as_obj=False)['description']
+
+
+def test_submission_params(datastore, client):
     user_id = random_id_from_collection(datastore, 'user')
 
     res = client.user.submission_params(user_id)
@@ -32,3 +157,32 @@ def test_get_submission_params(datastore, client):
     assert not {'download_encoding', 'hide_raw_results'}.issubset(set(res.keys()))
     assert {'deep_scan', 'groups', 'ignore_cache', 'submitter'}.issubset(set(res.keys()))
     assert res['submitter'] == user_id
+
+
+def test_tos(datastore, client):
+    res = client.user.tos('admin')
+    assert res['success']
+    assert datastore.user.get('admin', as_obj=False)['agrees_with_tos'] is not None
+
+    with pytest.raises(ClientError):
+        client.user.tos('user')
+
+
+def test_update(datastore, client):
+    new_name = "TEST_NAME!"
+
+    user_id = 'user'
+    user_data = datastore.user.get(user_id, as_obj=False)
+    assert user_data is not None
+
+    user_data['name'] = new_name
+    res = client.user.update(user_id, user_data)
+    assert res['success']
+
+    assert datastore.user.get(user_id, as_obj=False)['name'] == new_name
+
+
+def test_whoami(datastore, client):
+    res = client.user.whoami()
+    assert {'c12nDef', 'configuration', 'username', 'indexes'}.issubset(set(res.keys()))
+    assert res['username'] == 'admin'
