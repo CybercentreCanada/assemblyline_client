fdfca63d9ab5a3437f5e1f1c873aa5ca1fd1b889
Steve Garon <steve.garon@cyber.gc.ca>
2022-04-26 16:39:01 +0000

    Added tests for raw output

--- test/test_ontology.py
+++ test/test_ontology.py
@@ -1,6 +1,8 @@
 
 import pytest
 
+from io import BytesIO
+
 try:
     from utils import random_id_from_collection
 except ImportError:
@@ -19,6 +21,15 @@ def test_get_ontology_for_alert(datastore, client):
     assert any([record['header']['sha256'] == alert_data['file']['sha256'] for record in res])
 
 
+def test_get_ontology_for_alert_raw(datastore, client):
+    alert_id = random_id_from_collection(datastore, 'alert')
+
+    bio = BytesIO()
+    res = client.ontology.alert(alert_id, output=bio)
+    assert res
+    assert bio.getbuffer().nbytes > 0
+
+
 def test_get_ontology_for_file(datastore, client):
     sid = random_id_from_collection(datastore, 'submission')
     submission_data = datastore.submission.get(sid, as_obj=False)
@@ -28,9 +39,29 @@ def test_get_ontology_for_file(datastore, client):
     assert all([record['header']['sha256'] == sha256 for record in res])
 
 
+def test_get_ontology_for_file_raw(datastore, client):
+    sid = random_id_from_collection(datastore, 'submission')
+    submission_data = datastore.submission.get(sid, as_obj=False)
+    sha256 = submission_data['files'][0]['sha256']
+
+    bio = BytesIO()
+    res = client.ontology.file(sha256, output=bio)
+    assert res
+    assert bio.getbuffer().nbytes > 0
+
+
 def test_get_ontology_for_submission(datastore, client):
     sid = random_id_from_collection(datastore, 'submission')
     submission_data = datastore.submission.get(sid, as_obj=False)
     res = client.ontology.submission(sid)
     assert len(res) != 0
     assert any([record['header']['sha256'] == submission_data['files'][0]['sha256'] for record in res])
+
+
+def test_get_ontology_for_submission_raw(datastore, client):
+    sid = random_id_from_collection(datastore, 'submission')
+
+    bio = BytesIO()
+    res = client.ontology.submission(sid, output=bio)
+    assert res
+    assert bio.getbuffer().nbytes > 0
