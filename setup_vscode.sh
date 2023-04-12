59198368c6014d16abedb2646bbfc4e1c45a932a
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2023-04-12 20:12:04 +0000

    Removing codecov usage

--- setup_vscode.sh
+++ setup_vscode.sh
@@ -3,5 +3,5 @@
 python3 -m venv venv
 venv/bin/pip install -U pip
 venv/bin/pip install -U wheel
-venv/bin/pip install -U pytest pytest-cov codecov flake8 pep8 autopep8 ipython cart assemblyline passlib
+venv/bin/pip install -U pytest flake8 pep8 autopep8 ipython cart assemblyline passlib
 venv/bin/pip install -e .
