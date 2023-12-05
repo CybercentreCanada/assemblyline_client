4c7d015e0124d57f7859eb9df93c43cc7e416e60
gdesmar <75089569+gdesmar@users.noreply.github.com>
2023-12-05 19:12:45 +0000

    Same handling for all services and sha256s

--- assemblyline_client/v4_client/module/ontology.py
+++ assemblyline_client/v4_client/module/ontology.py
@@ -1,13 +1,19 @@
 import json
-
+from typing import List, Union
 from assemblyline_client.v4_client.common.utils import api_path, raw_output, stream_output
+from assemblyline_client import Connection
 
 
 class Ontology(object):
-    def __init__(self, connection):
-        self._connection = connection
+    def __init__(self, connection: Connection):
+        self._connection: Connection = connection
 
-    def alert(self, alert_id, sha256s=[], services=[], output=None):
+    def alert(
+            self,
+            alert_id: str,
+            sha256s: Union[List[str], str] = [],
+            services: Union[List[str], str] = [],
+            output=None):
         """\
 WARNING:
     This APIs output is considered stable but the ontology model itself is still in its
@@ -19,16 +25,25 @@ Required:
 alert_id     : Alert ID to get ontology records for (string)
 
 Optional:
-sha256s       : List of sha256 to get ontology records for (strings - default: all)
-services      : List of services to get ontology records for (strings - Default: all)
+sha256s       : Single or list of sha256 to get ontology records for (strings - default: all)
+services      : Single or list of services to get ontology records for (strings - Default: all)
 output        : Output stream that will receive the raw data of
                 the API instead of json loading every record (file handle or BytesIO)
 
 Throws a Client exception if the alert or submission does not exist.
 """
         params_tuples = []
-        params_tuples.extend([('sha256', x) for x in sha256s])
-        params_tuples.extend([('service', x) for x in services])
+        if isinstance(sha256s, str):
+            # Assume the user wanted a single sha256 and did not use a list
+            params_tuples.extend([('sha256', sha256s)])
+        else:
+            params_tuples.extend([('sha256', x) for x in sha256s])
+
+        if isinstance(services, str):
+            # Assume the user wanted a single service and did not use a list
+            params_tuples.extend([('service', services)])
+        else:
+            params_tuples.extend([('service', x) for x in services])
 
         kw = {}
         if params_tuples:
@@ -40,7 +55,7 @@ Throws a Client exception if the alert or submission does not exist.
         data = self._connection.download(api_path('ontology', 'alert', alert_id, **kw), raw_output)
         return [json.loads(line) for line in data.splitlines()]
 
-    def file(self, sha256, services=[], all=False, output=None):
+    def file(self, sha256: str, services: Union[List[str], str] = [], all: bool = False, output=None):
         """\
 WARNING:
     This APIs output is considered stable but the ontology model itself is still in its
@@ -52,7 +67,7 @@ Required:
 sha256     : SHA256 hash to get ontology records for (string)
 
 Optional:
-services     : List of services to get ontology records for (strings - default: all)
+services     : Single or list of services to get ontology records for (strings - default: all)
 all          : If there are multiple version of the ontology records, get them all (bool)
 output       : Output stream that will receive the raw data of
                 the API instead of json loading every record (file handle or BytesIO)
@@ -75,7 +90,12 @@ Throws a Client exception if the file does not exist.
         data = self._connection.download(api_path('ontology', 'file', sha256, **kw), raw_output)
         return [json.loads(line) for line in data.splitlines()]
 
-    def submission(self, sid, sha256s=[], services=[], output=None):
+    def submission(
+            self,
+            sid: str,
+            sha256s: Union[List[str], str] = [],
+            services: Union[List[str], str] = [],
+            output=None):
         """\
 WARNING:
     This APIs output is considered stable but the ontology model itself is still in its
@@ -87,16 +107,26 @@ Required:
 sid     : Submission ID to get ontology records for (string)
 
 Optional:
-sha256s       : List of sha256 to get ontology records for (Default: all)
-services      : List of services to get ontology records for (Default: all)
+sha256s       : Single or list of sha256 to get ontology records for (Default: all)
+services      : Single or list of services to get ontology records for (Default: all)
 output        : Output stream that will receive the raw data of
                 the API instead of json loading every record (file handle or BytesIO)
 
 Throws a Client exception if the submission does not exist.
 """
         params_tuples = []
-        params_tuples.extend([('sha256', x) for x in sha256s])
-        params_tuples.extend([('service', x) for x in services])
+
+        if isinstance(sha256s, str):
+            # Assume the user wanted a single sha256 and did not use a list
+            params_tuples.extend([('sha256', sha256s)])
+        else:
+            params_tuples.extend([('sha256', x) for x in sha256s])
+
+        if isinstance(services, str):
+            # Assume the user wanted a single service and did not use a list
+            params_tuples.extend([('service', services)])
+        else:
+            params_tuples.extend([('service', x) for x in services])
 
         kw = {}
         if params_tuples:
