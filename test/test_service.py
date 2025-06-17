eb5d5cf1cb77ec04027da50801eb6dd13154ab70
Shelly Wang <ching-wen.wang@cyber.gc.ca>
2025-06-17 13:47:41 +0000

    fix service backup test to use the full flag

--- test/test_service.py
+++ test/test_service.py
@@ -1,4 +1,3 @@
-
 import pytest
 import requests
 import yaml
@@ -11,6 +10,7 @@ try:
     from utils import random_id_from_collection
 except ImportError:
     import sys
+
     if sys.version_info < (3, 0):
         pytestmark = pytest.mark.skip
     else:
@@ -19,47 +19,48 @@ except ImportError:
 
 def test_add_service(datastore, client):
     resp = requests.get(
-        'https://raw.githubusercontent.com/CybercentreCanada/assemblyline-v4-service/master/'
-        'assemblyline_result_sample_service/service_manifest.yml')
+        "https://raw.githubusercontent.com/CybercentreCanada/assemblyline-v4-service/master/"
+        "assemblyline_result_sample_service/service_manifest.yml"
+    )
     res = client.service.add(resp.text)
-    assert res['service_name'] == 'ResultSample'
+    assert res["service_name"] == "ResultSample"
 
-    service_data = datastore.get_service_with_delta('ResultSample', as_obj=False)
+    service_data = datastore.get_service_with_delta("ResultSample", as_obj=False)
     assert service_data
-    assert service_data['name'] == 'ResultSample'
+    assert service_data["name"] == "ResultSample"
 
     # Cleanup so the new service does not interfere with other tests
-    datastore.service_delta.delete('ResultSample')
-    datastore.service.delete_by_query('name:ResultSample')
+    datastore.service_delta.delete("ResultSample")
+    datastore.service.delete_by_query("name:ResultSample")
     datastore.service.commit()
     datastore.service_delta.commit()
 
 
 def test_backup(datastore, client):
-    random_service = random_id_from_collection(datastore, 'service_delta')
+    random_service = random_id_from_collection(datastore, "service_delta")
     delta = datastore.service_delta.get(random_service, as_obj=False)
     version = datastore.service.get(f"{random_service}_{delta['version']}", as_obj=False)
 
     res = client.service.backup()
     backup_data = yaml.safe_load(res)
-    assert backup_data['type'] == 'backup'
-    assert isinstance(backup_data['data'], dict)
+    assert backup_data["type"] == "backup"
+    assert isinstance(backup_data["data"], dict)
 
-    backup = backup_data['data'].get(random_service, {})
-    assert backup.get('config', None) == delta
-    assert backup.get('versions', {}).get(f"{random_service}_{delta['version']}", None) == version
+    backup = backup_data["data"].get(random_service, {})
+    assert backup.get("config", None) == delta
+    assert backup.get("versions", {}).get(f"{random_service}_{delta['version']}", None) == version
 
 
 def test_constants(client):
     res = client.service.constants()
-    assert 'categories' in res
-    assert 'stages' in res
+    assert "categories" in res
+    assert "stages" in res
 
 
 def test_delete_service(datastore, client):
-    random_service = random_id_from_collection(datastore, 'service_delta')
+    random_service = random_id_from_collection(datastore, "service_delta")
     res = client.service.delete(random_service)
-    assert res['success']
+    assert res["success"]
 
     datastore.service_delta.commit()
     datastore.service.commit()
@@ -67,20 +68,20 @@ def test_delete_service(datastore, client):
 
 
 def test_get_service(datastore, client):
-    random_service = random_id_from_collection(datastore, 'service_delta')
+    random_service = random_id_from_collection(datastore, "service_delta")
     res = client.service(random_service)
-    assert res['name'] == random_service
+    assert res["name"] == random_service
 
     with pytest.raises(ClientError):
-        client.service(random_service, version='5.0.0')
+        client.service(random_service, version="5.0.0")
 
     version = f"{FRAMEWORK_VERSION}.{SYSTEM_VERSION}.{BUILD_MINOR}.1"
     res = client.service(random_service, version=version)
-    assert res['version'] == version
+    assert res["version"] == version
 
 
 def test_get_service_versions(datastore, client):
-    random_service = random_id_from_collection(datastore, 'service_delta')
+    random_service = random_id_from_collection(datastore, "service_delta")
     res = client.service.versions(random_service)
     assert len(res) >= 1
     for v in res:
@@ -89,14 +90,18 @@ def test_get_service_versions(datastore, client):
 
 def test_list_services(datastore, client):
     res = client.service.list()
-    service_list = [srv['name'] for srv in datastore.list_all_services(as_obj=False)]
+    service_list = [srv["name"] for srv in datastore.list_all_services(as_obj=False)]
     for srv in res:
-        assert srv['name'] in service_list
+        assert srv["name"] in service_list
 
 
 def test_restore_backup(datastore, client):
-    original_data = datastore.list_all_services(full=True, as_obj=False).sort(key=lambda x: x['name'])
-    backup = client.service.backup()
+    original_data = datastore.list_all_services(full=True, as_obj=False)
+    original_data.sort(key=lambda x: x["name"])
+
+    original_service_versions = set(datastore.service.keys("id:*"))
+
+    backup = client.service.backup(full=True)
 
     datastore.service.wipe()
     datastore.service_delta.wipe()
@@ -107,38 +112,47 @@ def test_restore_backup(datastore, client):
     assert len(datastore.list_all_services(full=True, as_obj=False)) == 0
 
     res = client.service.restore(backup)
-    assert res['success']
+    assert res["success"]
 
     datastore.service.commit()
     datastore.service_delta.commit()
 
-    restored_data = datastore.list_all_services(full=True, as_obj=False).sort(key=lambda x: x['name'])
+    restored_service_versions = set(datastore.service.keys("id:*"))
+
+    restored_data = datastore.list_all_services(full=True, as_obj=False)
+    restored_data.sort(key=lambda x: x["name"])
+
     assert original_data == restored_data
+    assert original_service_versions == restored_service_versions
 
 
 def test_set_service(datastore, client):
-    random_service = random_id_from_collection(datastore, 'service_delta')
+    random_service = random_id_from_collection(datastore, "service_delta")
     current_service_config = datastore.get_service_with_delta(random_service, as_obj=False)
 
     new_service_config = deepcopy(current_service_config)
-    new_service_config['enabled'] = not current_service_config['enabled']
+    new_service_config["enabled"] = not current_service_config["enabled"]
     res = client.service.set(random_service, new_service_config)
-    assert res['success']
+    assert res["success"]
 
     datastore.service_delta.commit()
 
     updated_service_config = datastore.get_service_with_delta(random_service, as_obj=False)
-    assert current_service_config['enabled'] != updated_service_config['enabled']
+    assert current_service_config["enabled"] != updated_service_config["enabled"]
 
-    assert datastore.service_delta.get(random_service)['enabled'] == new_service_config['enabled']
+    assert datastore.service_delta.get(random_service)["enabled"] == new_service_config["enabled"]
 
 
 def test_update(datastore, client):
-    random_service = random_id_from_collection(datastore, 'service_delta')
+    random_service = random_id_from_collection(datastore, "service_delta")
+    random_service = "Beaver"
     version = f"{FRAMEWORK_VERSION}.{SYSTEM_VERSION}.{BUILD_MINOR}.1"
     res = client.service.update(random_service, f"cccs/assemblyline-service-extract:{version}", version)
-    assert res['success']
-    assert res['status'] == 'updated'
+    print(f"{random_service}_{version}")
+    print(random_service)
+    print(res)
+    assert res["success"]
+    assert res["status"] == "updated"
 
 
 def test_updates(client):
